55bcdb73980c1a2a9bf1d57f0d991415788d23a6
raihan0504 <mhd.raihan0504@gmail.com>
2025-05-15 01:06:32 +0700

    update

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float moveSpeed;
+    [SerializeField] float detectionRadius;
+    [SerializeField] LayerMask playerLayer;
+    [SerializeField] Transform player;
 
-    // Update is called once per frame
-    void Update()
+    private bool canSeePlayer = false;
+
+    private void Update()
     {
-        
+        if (player == null) return;
+
+        // Hitung jarak
+        float distance = Vector2.Distance(transform.position, player.position);
+
+        // Check Player Dalam Radius
+        if (distance <= moveSpeed)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, player.position - transform.position, detectionRadius, playerLayer);
+            if (hit.collider != null && hit.collider.CompareTag("Player"))
+            {
+                canSeePlayer = true;
+            } else
+            {
+                canSeePlayer = false;
+            }
+        } else
+        {
+            canSeePlayer= false;
+        }
+        // Kejar Player
+        if (canSeePlayer)
+        {
+            Vector2 dir = (player.position - transform.position).normalized;
+            transform.position += (Vector3)(dir * moveSpeed * Time.deltaTime);
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = canSeePlayer ? Color.red : Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
     }
 }
